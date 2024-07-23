d0a3367b501d9ee6c02f0192a5b006f780cd8512
calebroelens <calebroelens@hotmail.com>
2024-07-23 21:30:22 +0200

    [REF] Fully refactored website / New layout and pages.

--- script/index.js
+++ script/index.js
@@ -1,36 +1,77 @@
 
-function playerOnlineEntry(playerName, UUID) {
-    let fallbackUUIDs =  {
-        "DaddyMarvel": "d49ef0b1e37d41d680a7f4fe74a57696"
-    }
-    let playerUUID = UUID;
-    if(playerName in fallbackUUIDs){
-        playerUUID = fallbackUUIDs[playerName];
-    }
-    return `
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <p class="card-title">${playerName}</p>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-4">
-                            <img alt="Could not get skin image" src="https://crafatar.com/renders/body/${playerUUID}">
-                        </div>
-                        <div class="col-auto">
-                            <strong>Username</strong>
-                            <p>${playerName}</p>
-                            <strong>Get skin</strong>
-                            <p>
-                                <a href="https://crafatar.com/skins/${playerUUID}" class="btn btn-success">Download</a>
-                            </p>
+let playerOnlineEntryV2 = (identifier, playerData) => {
+    return `<div class="column is-one-third" id="${identifier}">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">${playerData.playerName}</p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content is-flex is-justify-content-center">
+                            <img src="https://crafatar.com/renders/body/${playerData.playerUUID}" alt="Skin" style="max-height: 12rem;">
                         </div>
                     </div>
+                    <footer class="card-footer">
+                        <a href="https://crafatar.com/skins/${playerData.playerUUID}" class="card-footer-item"><i class="fa fa-download"></i>&nbsp;Download skin</a>
+                    </footer>
                 </div>
-            </div>
-        </div>
-    `;
+            </div>`;
+}
+
+let playerOnlineEntryV2_Loading = (identifier) => {
+    return `<div class="column is-one-third" id="${identifier}">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title" id="${identifier}_username">Loading</p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content is-flex is-justify-content-center" id="${identifier}_image">
+                            <i class="fa fa-bounce fa-spinner"></i>
+                        </div>
+                    </div>
+                    <footer class="card-footer" id="${identifier}_download">
+                        
+                    </footer>
+                </div>
+            </div>`;
+}
+
+let updateOnlinePlayersHTML = (players) => {
+    document.getElementById("current_player_count").innerHTML = players['now'] || "0";
+    document.getElementById("max_player_count").innerHTML = players['max'] || "16";
+    let progressBar = document.getElementById("player_progress_bar");
+    progressBar.value = players['now'] || 0;
+    progressBar.max = players['max'] || 16;
+}
+
+let updateOnlinePlayersTableHTML = async (players) => {
+    let playerList = players.list;
+    let playerListEl = document.getElementById("player_list");
+    /* Prefill to shown loading containers */
+    let identifiers = [];
+    playerList.forEach((player) => identifiers.push(`${player}_entry`));
+    playerListEl.innerHTML = "";
+    for(let identifier of identifiers){
+        playerListEl.innerHTML += playerOnlineEntryV2_Loading(identifier);
+    }
+    for(let [index, player] of Object.entries(playerList)){
+        let playerData = await getPlayerProfile(player);
+        let identifier = identifiers[index];
+        document.getElementById(`${identifier}_username`).innerHTML
+            = `<p class="card-header-title">${playerData.playerName}</p>`;
+
+        document.getElementById(`${identifier}_image`).innerHTML
+            = `<img src="https://crafatar.com/renders/body/${playerData.playerUUID}" alt="Skin" style="max-height: 12rem;">`;
+
+        document.getElementById(`${identifier}_download`).innerHTML
+            = `<a href="https://crafatar.com/skins/${playerData.playerUUID}" class="card-footer-item"><i class="fa fa-download"></i>&nbsp;Download skin</a>`;
+    }
+}
+
+let getPlayerProfile = async (playerName) => {
+    let response = await fetch(`https://api.ashcon.app/mojang/v2/user/${playerName}`);
+    let jsonData = await response.json();
+    let playerUUID = jsonData["uuid"];
+    return {playerName: playerName, playerUUID: playerUUID};
 }
 
 let handleUpdateServerStatus = async (err, status) => {
@@ -38,20 +79,12 @@ let handleUpdateServerStatus = async (err, status) => {
         console.error("Failed to load server status: ", err);
     }
     else{
-        document.getElementById("player_count").innerHTML = `${status['players']['now']} / ${status['players']['max']}`;
-        document.getElementById("server_version").innerHTML = status['version'];
-        let playerTable = document.getElementById("player_list");
-        for(let player of status['players']['list']){
-            let response = await fetch(`https://api.ashcon.app/mojang/v2/user/${player}`);
-            let json_data = await response.json();
-            let playerUUID = json_data["uuid"];
-            playerTable.innerHTML += playerOnlineEntry(player, playerUUID);
-        }
+        updateOnlinePlayersHTML(status['players']);
+        await updateOnlinePlayersTableHTML(status['players']);
     }
 }
 
 let updateServerStatus = () => {
-    console.log("Pinging...");
     MinecraftAPI.getServerQuery("callippoocraft.cloudnord.net", {} , handleUpdateServerStatus);
 }
 
